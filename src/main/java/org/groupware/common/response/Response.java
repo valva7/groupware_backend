9937e1b0c421af378da31e1b2fddd8550187398e
kimgleam <valva7@naver.com>
2025-09-24 18:48:30 +0900

    refactor : Response 객체 수정

--- src/main/java/org/groupware/common/response/Response.java
+++ src/main/java/org/groupware/common/response/Response.java
@@ -3,7 +3,7 @@ package org.groupware.common.response;
 
 import org.groupware.global.exception.ErrorCode;
 
-public record Response<T>(Integer code, String message, T value) {
+public record Response<T>(Integer code, String message, T data) {
 
     public static <T> Response<T> ok(T value){
         return new Response<>(0, "ok", value);
